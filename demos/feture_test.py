5115be912249855c58a19ab4ae81035f3afd9d0a
Manan mehta <mehtamanan0@gmail.com>
2018-09-14 12:58:34 +0530

    Update feture_test.py

--- demos/feture_test.py
+++ demos/feture_test.py
@@ -3,6 +3,8 @@ import librosa, librosa.display
 original_song = "../data/test_data/test.mp3"
 cover_song = "../data/test_data/test.mp3"
 
+signal = librosa.load(original_song)[0]
+
 def extract_features(signal):
     return [
         librosa.feature.zero_crossing_rate(signal)[0, 0],
