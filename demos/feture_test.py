906a50a5eeb442e91e114bf914175cccedeb34ff
manan mehta <mehtamanan0@gmail.com>
2018-09-22 10:57:22 +0530

    Add all features

--- demos/feture_test.py
+++ demos/feture_test.py
@@ -9,6 +9,19 @@ def extract_features(signal):
     return [
         librosa.feature.zero_crossing_rate(signal)[0, 0],
         librosa.feature.spectral_centroid(signal)[0, 0],
+        librosa.feature.spectral_bandwidth(signal)[0, 0],
+        librosa.feature.spectral_contrast(signal)[0, 0],
+        librosa.feature.spectral_flatness(signal)[0, 0],
+        librosa.feature.spectral_rolloff(signal)[0, 0],
+        librosa.feature.chroma_stft(signal)[0, 0],
+        librosa.feature.chroma_cqt(signal)[0, 0],
+        librosa.feature.chroma_cens(signal)[0, 0],
+        librosa.feature.melspectrogram(signal)[0, 0],
+        librosa.feature.mfcc(signal)[0, 0],
+        librosa.feature.rmse(signal)[0, 0],
+        librosa.feature.poly_features(signal)[0, 0],
+        librosa.feature.tonnetz(signal)[0, 0],
+        librosa.feature.zero_crossing_rate(signal)[0, 0]
     ]
 
-
+extract_features(signal)
