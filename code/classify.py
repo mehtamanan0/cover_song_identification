a763281b637b5aef78cb91d4bde90fafe181125e
Manan Mehta <mehtamanan0@gmail.com>
2019-01-02 20:01:40 +0530

    add testing file

---
+++ code/classify.py
@@ -0,0 +1,48 @@
+from sklearn.externals import joblib
+import librosa, librosa.display
+
+import sys
+
+def oti_func(original_features, cover_features):
+    profile1 = np.sum(original_features.T, axis = 1)
+    profile2 = np.sum(cover_features.T, axis = 1)
+    oti = [0] * 12
+    for i in range(profile2.shape[0]):
+        oti[i] = np.dot(profile1, np.roll(profile2, i))
+    oti.sort(reverse=True)
+    newmusic = np.roll(cover_features.T, int(oti[0]))
+    return newmusic.T
+
+def extract_features(signal):
+    return librosa.feature.chroma_stft(signal)[:12, :180].T
+
+def sim_matrix(original_features, cover_features):
+    similarity_matrix = []
+    for each in original_features:
+        sm = []
+        for beach in cover_features:
+            sm.append(euclidean(each, beach))
+        similarity_matrix.append(sm)
+    return np.array(similarity_matrix)
+
+model = joblib.load("../data/models/model.pkl")
+
+original_song = sys.argv[1]
+cover_song = sys.argv[2]
+
+original_signal = librosa.load(original_song)[0]
+cover_signal = librosa.load(cover_song)[0]
+
+original_features = extract_features(original_signal)
+cover_features = extract_features(cover_signal)
+
+oti_cover_features = oti_func(original_features, cover_features)
+
+mat = sim_matrix(original_features, oti_cover_features)
+
+if mat.shape[0] < 180:
+    mat = np.pad(mat, ((0,180 - mat.shape[0]),(0,0)), mode = 'constant', constant_values=0)
+if mat.shape[1] < 180:
+    mat = np.pad(mat, ((0,0),(0,180 - mat.shape[1])), mode = 'constant', constant_values=0)
+
+print(model.predect(mat))
