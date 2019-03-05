8df304a4fb208e7d9878aba1da4115d16f5a1762
Manan Mehta <mehtamanan0@gmail.com>
2019-03-05 11:44:37 +0530

    fix directory issue

--- cover_inception/convert.py
+++ cover_inception/convert.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import numpy as np
 import sys
 import os
 from PIL import Image
 from tqdm import tqdm
 
 label = sys.argv[1]
-lis = os.listdir("../data/csm/{}".format(label))
+direc = "../data/csm/{}".format(label)
+lis = os.listdir(direc)
 p_bar = tqdm(total = len(lis))
 
 for each in lis:
     p_bar.update(1)
-    val = pd.read_csv(each, header=None).values
+    val = pd.read_csv(os.path.join(direc, each), header=None).values
     img = Image.fromarray(np.uint8(val * 255) , 'L')
     img.save("training_dataset/{}/{}{}".format(label,each.split(".")[0], '.jpg'))
 
